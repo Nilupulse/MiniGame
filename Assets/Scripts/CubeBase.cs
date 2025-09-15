e32651ca5e1e488c113082a75f75c3cd89aaba1c
Nilupul Senadheera <nilupulse@gmail.com>
2025-09-14 22:46:43 -0400

    Cube base added.

---
+++ Assets/Scripts/CubeBase.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public abstract class CubeBase : MonoBehaviour
+{
+    [Header("Display Materials")]
+    [SerializeField] protected Material displayMaterial;
+    [SerializeField] protected Material letterMaterial;
+    [Header("Display Colors")]
+    [SerializeField] protected Color letterColour;
+    [SerializeField] protected Color displayColour;
+    [Header("Flash Configuration")]
+    [SerializeField] protected FlashConfiguration flashConfig;
+    [Header("Handlers")]
+    [SerializeField] protected SnapCubes snapCubes;
+    [SerializeField] protected InputHandler inputHandler;
+    [SerializeField] protected ColorFlashHandler flashHandler;
+
+    public void ResetDisplay()
+    {
+        displayMaterial.color = displayColour;
+    }
+
+    public void SetUpColour()
+    {
+        displayMaterial.color = displayColour;
+        letterMaterial.color = letterColour;
+    }
+}
